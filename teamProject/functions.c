c5d53c22adb072364b4617d7c0580c5473e2d22b
dchine21 <70595559+dchine21@users.noreply.github.com>
2021-04-28 11:10:07 -0400

    Add files via upload

---
+++ teamProject/functions.c
@@ -0,0 +1,59 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "functions.h"
+
+
+double** alloc2d(int nrows, int ncols) {
+   // first allocate a block of memory for the row pointers
+   double **x = (double **)malloc(nrows*sizeof(double *));
+   // Now allocate a block of memory for the 2D array
+   x[0] = (void *)malloc(nrows*ncols*sizeof(double));
+   // Last, assign the memory location to point to in the block of data for each row pointer
+   for (int j = 1; j < nrows; j++) {
+      x[j] = x[j-1] + ncols;
+   }
+   return x;
+}
+
+void alloc2dnew(double*** a, int nrows, int ncols) {
+   // first allocate a block of memory for the row pointers
+   double **x = (double **)malloc(nrows*sizeof(double *));
+   // Now allocate a block of memory for the 2D array
+   x[0] = (void *)malloc(nrows*ncols*sizeof(double));
+   // Last, assign the memory location to point to in the block of data for each row pointer
+   for (int j = 1; j < nrows; j++) {
+      x[j] = x[j-1] + ncols;
+   }
+
+   *a = x;
+}
+
+void fill2d(double** a, int nrows, int ncols) {
+    for(int i=0; i<nrows; i++) {
+        for(int j=0; j<ncols; j++) {
+            a[i][j] = j+i*ncols;
+        }
+    }
+}
+
+void createBStart(double** b, int nrows, int ncols) {
+    for(int i=0; i<nrows; i++) {
+        for(int j=0; j<ncols; j++) {
+            if(j == 0 || j == ncols-1)
+                b[i][j] = 1;
+            else
+                b[i][j] = 0;
+        }
+    }
+}
+
+void print2d(double** a, int nrows, int ncols) {
+    for(int i=0; i<nrows; i++) {
+        for(int j=0; j<ncols; j++) {
+            printf("%.2f\t", a[i][j]);
+        } printf("\n");
+    }
+    printf("\n");
+}
